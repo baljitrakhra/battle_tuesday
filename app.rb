644c5d14e4b3b05ca931722bb9cfb375f4e6784f
Baljit Rakhra <baljit.rakhra@gmail.com>
2022-04-05 17:50:05 +0100

    Enterying players and diplying their names

--- app.rb
+++ app.rb
@@ -10,7 +10,14 @@ class Battle < Sinatra::Base
     "Testing infrastructure working!"
   end
 
-
+  get '/name-form' do
+    erb(:form)
+  end
+  post '/name' do
+    p @p1name = params[:p1name]
+    p @p2name = params[:p2name]
+    erb(:play)
+  end
 # # Start the server if this file is executed directly (do not change the line below)
 run! if app_file == $0
 
--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -9,3 +9,10 @@ feature 'Testing infrastructure' do
     expect(page).to have_content("Testing infrastructure working!")
   end
 end
+
+feature 'expect players to enter their names' do
+  scenario'entering name in the box' do
+    visit('/name-form')
+    expect(page).to have_button("Submit")
+  end
+end
